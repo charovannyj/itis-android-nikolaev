fa0ce58ce2629583e4452cbc097907778181b512
Arslan Nikolaev <nikolaevarslan1@yandex.ru>
2023-10-08 18:09:26 +0300

    commit only for me and not for testing

--- app/src/main/java/ru/kpfu/itis/myapplication/SecondActivity.kt
+++ app/src/main/java/ru/kpfu/itis/myapplication/SecondActivity.kt
@@ -2,45 +2,20 @@ package ru.kpfu.itis.myapplication
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import ru.kpfu.itis.myapplication.ui.theme.MyApplicationTheme
+import java.util.Random
+
 
 class SecondActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            MyApplicationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
+        setContentView(R.layout.activity_second)
     }
-}
+    fun showRandomNumber(){
+        val count = intent.getIntExtra("st", 7)
+        val random = Random()
+        var randomInt = 0
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyApplicationTheme {
-        Greeting("Android")
+        R.id.textViewRandom = randomInt.toString().toInt()
+        R.id.textViewLabel = getString(R.string.random_heading,count).toInt()
     }
 }
