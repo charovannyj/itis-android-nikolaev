e0e7d0f41f9e94203c6a74c909013a23345b9487
Arslan Nikolaev <nikolaevarslan1@yandex.ru>
2023-10-11 22:31:19 +0300

    implemented the basics of the project, but it doesn't work

--- app/src/main/java/ru/kpfu/itis/myapplication/FirstFragment.kt
+++ app/src/main/java/ru/kpfu/itis/myapplication/FirstFragment.kt
@@ -5,10 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import androidx.fragment.app.FragmentTransaction
 import ru.kpfu.itis.myapplication.databinding.FragmentFirstBinding
 
 class FirstFragment : Fragment() {
-    private var binding: FragmentFirstBinding? = null
+    private var _binding: FragmentFirstBinding? = null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -19,4 +21,32 @@ class FirstFragment : Fragment() {
         }
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        _binding?.button?.setOnClickListener {
+            val text = _binding?.editText?.text.toString()
+            _binding!!.button.setOnClickListener{
+                requireActivity()
+                    .supportFragmentManager
+                    .beginTransaction()
+                    .add(R.id.fragment_container, ThirdFragment.getInstance(text), ThirdFragment.THIRD_FRAGMENT_TAG)
+                    .addToBackStack(null)
+                    .commit()
+                requireActivity()
+                    .supportFragmentManager
+                    .beginTransaction()
+                    .add(R.id.fragment_container, SecondFragment.getInstance(text), SecondFragment.SECOND_FRAGMENT_TAG)
+                    .addToBackStack(null)
+                    .commit()
+            }
+        }
+    }
+    companion object {
+        const val FIRST_FRAGMENT_TAG = "FIRST_FRAGMENT_TAG"
+        fun getInstance() = FirstFragment()
+        fun getInstance(text: String?): FirstFragment = FirstFragment().apply {
+            arguments = bundleOf(ParamsKey.MESSAGE_TEXT_KEY to text)
+        }
+    }
 }
--- app/src/main/java/ru/kpfu/itis/myapplication/SecondFragment.kt
+++ app/src/main/java/ru/kpfu/itis/myapplication/SecondFragment.kt
@@ -5,55 +5,59 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import ru.kpfu.itis.myapplication.databinding.FragmentFirstBinding
+import ru.kpfu.itis.myapplication.databinding.FragmentSecondBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [SecondFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SecondFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
+    private var _binding: FragmentSecondBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false)
+        return inflater.inflate(R.layout.fragment_second, container, false).also {
+            FragmentSecondBinding.bind(it)
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        _binding?.buttonTo3?.setOnClickListener {
+            requireActivity()
+                .supportFragmentManager
+                .beginTransaction()
+                .add(
+                    R.id.fragment_container,
+                    ThirdFragment.getInstance(_binding!!.textView.text.toString()),
+                    ThirdFragment.THIRD_FRAGMENT_TAG
+                )
+                .addToBackStack(null)
+                .commit()
+        }
+        _binding?.buttonTo1?.setOnClickListener {
+            requireActivity()
+                .supportFragmentManager
+                .beginTransaction()
+                .add(
+                    R.id.fragment_container,
+                    FirstFragment.getInstance(_binding!!.textView.text.toString()),
+                    FirstFragment.FIRST_FRAGMENT_TAG
+                )
+                .addToBackStack(null)
+                .commit()
+        }
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SecondFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SecondFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        const val SECOND_FRAGMENT_TAG = "SECOND_FRAGMENT_TAG"
+        fun getInstance() = SecondFragment()
+        fun getInstance(text: String?): SecondFragment = SecondFragment().apply {
+            arguments = bundleOf(ParamsKey.MESSAGE_TEXT_KEY to text)
+        }
     }
 }
