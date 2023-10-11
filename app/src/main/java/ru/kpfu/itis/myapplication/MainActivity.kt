ca8257d73ebd91fdfcfdbcce40990b07b9ac1b9e
Arslan Nikolaev <nikolaevarslan1@yandex.ru>
2023-10-11 22:31:19 +0300

    implemented the basics of the project, but it doesn't work

--- app/src/main/java/ru/kpfu/itis/myapplication/MainActivity.kt
+++ app/src/main/java/ru/kpfu/itis/myapplication/MainActivity.kt
@@ -1,19 +1,24 @@
 package ru.kpfu.itis.myapplication
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
+import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import ru.kpfu.itis.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var _binding: ActivityMainBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater).also {
+            setContentView(it.root)
+        }
+        supportFragmentManager.beginTransaction()
+            .add(_binding!!.fragmentContainer.id, FirstFragment())
+            .commit()
     }
 
-    fun randomMe(view: View){
-        val randomIntent = Intent(this,SecondActivity::class.java)
-        randomIntent.putExtra("st", 2)
-        startActivity(randomIntent)
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
---
+++ app/src/main/java/ru/kpfu/itis/myapplication/ParamsKey.kt
@@ -0,0 +1,5 @@
+package ru.kpfu.itis.myapplication
+
+object ParamsKey {
+    const val MESSAGE_TEXT_KEY = "MESSAGE_TEXT_KEY"
+}
