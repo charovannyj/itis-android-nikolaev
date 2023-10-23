f0e2cbf70eda89bf83ba30f7df1d66d376622e2e
Arslan Nikolaev <nikolaevarslan1@yandex.ru>
2023-10-23 17:44:20 +0300

    created 2nd branch and some xml

--- app/src/main/java/ru/kpfu/itis/myapplication/FirstFragment.kt
+++ app/src/main/java/ru/kpfu/itis/myapplication/FirstFragment.kt
@@ -1,11 +1,11 @@
 package ru.kpfu.itis.myapplication
 
+import android.text.TextWatcher
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.os.bundleOf
 import ru.kpfu.itis.myapplication.databinding.FragmentFirstBinding
 
 class FirstFragment : Fragment(R.layout.fragment_first) {
@@ -22,44 +22,14 @@ class FirstFragment : Fragment(R.layout.fragment_first) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        with(_binding!!){
-            button.setOnClickListener{
-                texti = editText.text.toString()
-                requireActivity()
-                    .supportFragmentManager
-                    .beginTransaction()
-                    .add(R.id.fragment_container, SecondFragment.getInstance(texti), SecondFragment.SECOND_FRAGMENT_TAG)
-                    .addToBackStack(null)
-                    .commit()
-                requireActivity()
-                    .supportFragmentManager
-                    .beginTransaction()
-                    .add(R.id.fragment_container, ThirdFragment.getInstance(texti), ThirdFragment.THIRD_FRAGMENT_TAG)
-                    .addToBackStack(null)
-                    .commit()
-            }
+        with(_binding) {
+            this?.etPhoneNumber?.addTextChangedListener();
+
         }
     }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
-    }
-    companion object {
-        const val FIRST_FRAGMENT_TAG = "FIRST_FRAGMENT_TAG"
-        fun getInstance() = FirstFragment()
-        fun getInstance(text: String?): FirstFragment = FirstFragment().apply {
-            arguments = bundleOf(ParamsKey.MESSAGE_TEXT_KEY to text)
-        }
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        val currentTexti = texti
-        outState.putString("savedStr", currentTexti)    }
-
-    override fun onViewStateRestored(savedInstanceState: Bundle?) {
-        super.onViewStateRestored(savedInstanceState)
-        val currentString = savedInstanceState?.getString("savedStr") ?: texti
-        texti = currentString
     }
 }
